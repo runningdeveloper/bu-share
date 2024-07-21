0e81ecbb22b98704e2c895603f522fce204a1ca6
Geoffrey Hunt <info@runningdeveloper.com>
2024-07-21 12:09:56 +0200

    make it work over multiple days

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Button, Title, Container, Flex, TextInput, Box } from "@mantine/core";
 import {
   IconBrandWhatsapp,
@@ -10,20 +7,11 @@ import {
 } from "@tabler/icons-react";
 import { useState } from "react";
 import { useLocalStorage, useClipboard } from "@mantine/hooks";
+import { differenceInHours } from "date-fns";
+import { DateTimePicker } from "@mantine/dates";
 
-function getCurrentLap(): number {
-  const raceStartHour = 9;
-  const now = new Date();
-  const currentHour = now.getHours();
-  const currentMinute = now.getMinutes();
-
-  let lapNumber = currentHour - raceStartHour;
-
-  if (currentMinute > 0) {
-    lapNumber += 1;
-  }
-
-  return lapNumber > 0 ? lapNumber : 0;
+function getCurrentLap(start: Date): number {
+  return differenceInHours(new Date(), start) + 1;
 }
 
 type Name = {
@@ -43,17 +31,25 @@ function sortNames(names: Name[]): Name[] {
 function App() {
   // const [count, setCount] = useState(0)
   // const theme = useMantineTheme();
-  const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(null as "date" | "name" | null);
   const [newName, setNewName] = useState("");
+  // const [newStart, setNewStart] = useState(new Date());
 
   const [names, setNames] = useLocalStorage({
     key: "names",
     defaultValue: [] as Name[],
   });
 
+  const [start, setStart] = useLocalStorage({
+    key: "start",
+    defaultValue: new Date(),
+  });
+
+  const startDate = new Date(start);
+
   const addName = (name: string) => {
     setNames([...names, { name, running: true }]);
-    setEdit(false);
+    setEdit(null);
   };
 
   const removeName = (name: string) => {
@@ -73,35 +69,35 @@ function App() {
 
   const clipboard = useClipboard({ timeout: 500 });
 
+  const currentLap = getCurrentLap(start);
+
   return (
     <Container size="xs">
-      <Flex
-        // gap="md"
-        // justify="center"
-        align="center"
-        direction="column"
-      >
-        {/* <Flex gap="xs" my="lg"> */}
-        {/* <IconRun size={48} stroke={2} /> */}
-
+      <Flex align="center" direction="column">
         <Title mt="lg" mb="-20px" pb={0} ta="center" size={80} order={1}>
-          LAP {getCurrentLap()}
+          LAP {currentLap}
         </Title>
-        <Title mb="lg" ta="center" size={20} order={2}>{getCurrentLap()*6.7} km</Title>
+        <Title mb="lg" ta="center" size={20} order={2}>
+          {currentLap * 6.7} km
+        </Title>
         {sortNames(names).map((name) => (
           <Flex key={name.name} gap="xs" mb="xs" miw="300" align="center">
             {" "}
-            {name.running ? <IconCircleCheckFilled
-              size={40}
-              stroke={2}
-              color="#59BE23"
-              onClick={() => toggleRunning(name.name)}
-            />:<IconCircleXFilled
-            color="#e84d50"
-            size={40}
-            stroke={2}
-            onClick={() => toggleRunning(name.name)}
-          />}{" "}
+            {name.running ? (
+              <IconCircleCheckFilled
+                size={40}
+                stroke={2}
+                color="#59BE23"
+                onClick={() => toggleRunning(name.name)}
+              />
+            ) : (
+              <IconCircleXFilled
+                color="#e84d50"
+                size={40}
+                stroke={2}
+                onClick={() => toggleRunning(name.name)}
+              />
+            )}{" "}
             <Title mb="-10px" ta="center" size={40} order={2}>
               {name.name}
             </Title>
@@ -109,7 +105,6 @@ function App() {
             <IconTrash stroke={2} onClick={() => removeName(name.name)} />
           </Flex>
         ))}
-        {/* </Flex> */}
         <Button
           my="lg"
           color="myColor"
@@ -117,57 +112,95 @@ function App() {
           radius="md"
           leftSection={<IconBrandWhatsapp size={30} />}
           onClick={() => {
-            const text = {text: `Lap ${getCurrentLap()} - ${getCurrentLap()*6.7} km 🚀\n` + names.map(n => `${n.running?"🟢":"🔴"} ${n.name}`).join("\n")}
-            try{
-              navigator.share(text)
-            }catch(e){
-              console.log(e)
-              clipboard.copy(text.text)
+            const text = {
+              text:
+                `Lap ${currentLap} - ${currentLap * 6.7} km done 🚀\n` +
+                names
+                  .map((n) => `${n.running ? "🟢" : "🔴"} ${n.name}`)
+                  .join("\n"),
+            };
+            try {
+              navigator.share(text);
+            } catch (e) {
+              console.log(e);
+              clipboard.copy(text.text);
             }
-            // if (navigator && navigator.canShare(text)){
-            //   navigator.share({text: "test"})
-            // }else{
-            //   console.log("can't share")
-            // }
-
           }}
         >
-          {clipboard.copied ? "Copied": "Share"}
+          {clipboard.copied ? "Copied" : "Share"}
         </Button>
 
-        {edit && (
-          <Flex gap="xs" my="lg" align="end">
-            <TextInput
-              size="lg"
-              radius="md"
-              label="Name"
-              // description="Input description"
-              placeholder="Joe"
-              value={newName}
-              onChange={(event) => setNewName(event.currentTarget.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") addName(newName);
-              }}
-            />
-            <Button
-              onClick={() => addName(newName)}
-              size="lg"
-              color="myColor"
-              radius="md"
-            >
-              Save
-            </Button>
-          </Flex>
+        {edit === "name" && (
+          <>
+            <Flex gap="xs" my="lg" align="end">
+              <TextInput
+                size="lg"
+                radius="md"
+                label="Name"
+                placeholder="Joe"
+                value={newName}
+                onChange={(event) => setNewName(event.currentTarget.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") addName(newName);
+                }}
+              />
+              <Button
+                onClick={() => addName(newName)}
+                size="lg"
+                color="myColor"
+                radius="md"
+              >
+                Save
+              </Button>
+            </Flex>
+          </>
+        )}
+        {edit === "date" && (
+          <>
+            <Flex gap="xs" my="lg" align="end">
+              <DateTimePicker
+                clearable
+                value={startDate}
+                label="Pick date and time"
+                placeholder="Pick date and time"
+                onChange={(value) => {
+                  if (value && value instanceof Date) setStart(value);
+                }}
+              />
+            </Flex>
+          </>
         )}
       </Flex>
       <Button
-        onClick={() => setEdit(!edit)}
+        onClick={() => {
+          if (edit === null) {
+            setEdit("date");
+          } else {
+            setEdit(null);
+          }
+        }}
         size="xs"
         color="myColor"
         variant="light"
         radius="md"
       >
         Edit
+      </Button>
+      {" "}
+      <Button
+        onClick={() => {
+          if (edit === null) {
+            setEdit("name");
+          } else {
+            setEdit(null);
+          }
+        }}
+        size="xs"
+        color="myColor"
+        variant="light"
+        radius="md"
+      >
+        Add Name
       </Button>
     </Container>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import './index.css'
 import "@mantine/core/styles.css";
+import '@mantine/dates/styles.css';
 import {
   MantineProvider,
   createTheme,
